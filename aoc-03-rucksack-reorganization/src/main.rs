f8a358d90e2295a02ad3241eb2fa970076e5f8ec
Cezary Grzanka <cezarygrzanka@gmail.com>
2023-07-04 19:55:37 +0200

    aoc-03 part 1 done

--- aoc-03-rucksack-reorganization/src/main.rs
+++ aoc-03-rucksack-reorganization/src/main.rs
@@ -1,3 +1,35 @@
+use std::{collections::HashSet, fs};
+
+fn to_priority(ch: char) -> Option<u8> {
+    match ch {
+        'a'..='z' => Some(ch as u8 - 90 - 6),
+        'A'..='Z' => Some(ch as u8 - 26 - 12),
+        _ => None,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read_to_string("vals").expect("Should be able to read file");
+    let lines = file.lines();
+
+    let mut sum: usize = 0;
+
+    for line in lines {
+        let (left, right) = line.split_at(line.len() / 2);
+
+        let common_letters: HashSet<u8> = left
+            .chars()
+            .clone()
+            .filter_map(|c| match right.contains(c) {
+                true => to_priority(c),
+                false => None,
+            })
+            .collect();
+
+        for entry in common_letters.into_iter() {
+            sum += entry as usize;
+        }
+    }
+
+    println!("sum: {:?}", sum);
 }
