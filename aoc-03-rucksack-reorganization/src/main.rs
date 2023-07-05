10d9fbe65538510a6cba365f821203d9bec7b1e5
Cezary Grzanka <cezarygrzanka@gmail.com>
2023-07-05 16:58:51 +0200

    aoc-03 pt2 finished

--- aoc-03-rucksack-reorganization/src/main.rs
+++ aoc-03-rucksack-reorganization/src/main.rs
@@ -8,27 +8,44 @@ fn to_priority(ch: char) -> Option<u8> {
     }
 }
 
+fn common_letters(a: &str, b: &str, c: &str) -> char {
+    let common_ab: HashSet<char> = a
+        .chars()
+        .filter_map(|ch| match b.contains(ch) {
+            true => Some(ch),
+            false => None,
+        })
+        .collect();
+
+    return c
+        .chars()
+        .find_map(|ch| match common_ab.contains(&ch) {
+            true => Some(ch),
+            false => None,
+        })
+        .unwrap();
+}
+
 fn main() {
     let file = fs::read_to_string("vals").expect("Should be able to read file");
     let lines = file.lines();
 
-    let mut sum: usize = 0;
+    let mut grouped_lines: Vec<Vec<&str>> = vec![];
+    let mut buff: Vec<&str> = vec![];
+    for (it, line) in lines.enumerate() {
+        buff.push(line);
 
-    for line in lines {
-        let (left, right) = line.split_at(line.len() / 2);
-
-        let common_letters: HashSet<u8> = left
-            .chars()
-            .clone()
-            .filter_map(|c| match right.contains(c) {
-                true => to_priority(c),
-                false => None,
-            })
-            .collect();
-
-        for entry in common_letters.into_iter() {
-            sum += entry as usize;
+        if (it + 1) % 3 == 0 {
+            grouped_lines.push(buff);
+            buff = vec![];
         }
+    }
+
+    let mut sum: usize = 0;
+    for group in grouped_lines {
+        let common = common_letters(group[0], group[1], group[2]);
+        let priority = to_priority(common).unwrap();
+        sum += priority as usize;
     }
 
     println!("sum: {:?}", sum);
