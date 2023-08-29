1ad3f50a542794cd062e6fcd7e21fd742f802b4e
Cezary Grzanka <cezarygrzanka@gmail.com>
2023-08-29 14:55:21 +0200

    day 1 revisited

--- aoc-01-calorie-counting/src/main.rs
+++ aoc-01-calorie-counting/src/main.rs
@@ -1,32 +1,33 @@
-use std::collections::BinaryHeap;
 use std::fs;
 
 fn main() {
     let file_path = "src/elves.txt";
-
     let contents = fs::read_to_string(file_path).expect("Should have been able to read file");
-
     let lines = contents.lines();
 
-    let mut current_elven = 0;
-
-    let mut elvens = BinaryHeap::<i32>::new();
-
-    lines.for_each(|line| {
-        if line == "" {
-            elvens.push(current_elven);
-
-            current_elven = 0;
-            return;
+    let mut acc = 0;
+    let mut max = [0, 0, 0];
+    for line in lines {
+        match line {
+            "" => match acc {
+                n if n > max[0] => {
+                    max[0] = n;
+                    acc = 0
+                }
+                n if n > max[1] => {
+                    max[1] = n;
+                    acc = 0
+                }
+                n if n > max[2] => {
+                    max[2] = n;
+                    acc = 0
+                }
+                _ => acc = 0,
+            },
+            n => acc += n.to_string().parse::<i32>().unwrap(),
         }
-
-        current_elven += line.to_string().parse::<i32>().unwrap();
-    });
-
-    let mut sum = 0;
-    for _ in 0..3 {
-        sum += elvens.pop().unwrap();
     }
 
-    println!("Top 3 elvens have {} calories", sum);
+    println!("Top 1: {}", max[0]);
+    println!("Top 3: {}", max[0] + max[1] + max[2]);
 }
